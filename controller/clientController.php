8c83a7edbcddef09cdc263aafafba4e942ee7378
Badara <bbabadara@gmail.com>
2026-05-13 22:00:25 +0000

    chargement de la vue lister les clients

---
+++ controller/clientController.php
@@ -0,0 +1,41 @@
+<?php
+$liste=function(){
+require_once(ROOT."view/clients/liste.php");
+
+};
+
+$ajout=function(){
+echo "je ajoute un client";
+};
+
+$detail=function(){
+echo "je detail un client";
+};
+
+$modifier=function(){
+echo "je modifie un client";
+};
+
+$supprimer=function(){
+echo "je supprime un client";
+};
+
+
+$actions=[
+    "liste"=>$liste,
+    "ajout"=>$ajout,
+    "detail"=>$detail,
+    "modifier"=>$modifier,
+    "supprimer"=>$supprimer
+    
+];
+ $action=$_REQUEST["action"]??"liste";
+ 
+ if (array_key_exists($action, $actions)) {
+         $actions[$action]();
+     }
+     else{
+         echo "page introuvable c client";
+         exit();
+}
+         
